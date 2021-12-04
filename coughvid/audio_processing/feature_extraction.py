fixed normalization and filtering parameters

coughvid/audio_processing/feature_extraction.py
@@ -20,7 +20,11 @@ def normalize_audio(audio):
     '''Normalize the audio signal according to the formula in
     https://www.sciencedirect.com/science/article/pii/S0010482521003668?via%3Dihub
     '''
-    return 0.9 * audio / max(abs(np.max(audio)), 1e-6)
+    max_audio = abs(np.max(audio))
+    if max_audio <= 1e-6:
+        return 0.9 * audio
+
+    return 0.9 * audio / max_audio
 
 
 def zcr(frame):
@@ -53,9 +57,6 @@ def extract_frames(valid_samples, num_frames, frame_length, uuid=None):
         frame = valid_samples[int(i):int(i)+frame_length]
 
         if len(frame) < frame_length:
-            print(f'WARNING: Unexpected frame length encountered at {int(i)}'
-                  f' of {len(valid_samples)}: {len(frame)}. '
-                  f'Padding {frame_length-len(frame)} frames.')
 
             frame = np.pad(frame,
                            (0, max(0, frame_length-len(frame))), 'constant')

data_augmentation/data_augmentation.py
@@ -8,10 +8,10 @@ class DataAugmentation():
 
         self.audio = audio
 
-        # TODO: still need to figure out how to choose the filter requirements.
-        self.fs = 30.0  # sample rate, Hz
-        self.cutoff = 3  # desired cutoff frequency of the filter, Hz (the higher cutoff, the less effects applied for lowpass
-        self.order = 2  # sin wave can be approx represented as quadratic
+        self.fs = 44.1
+        self.lp_cutoff = 3
+        self.hp_cutoff = 2
+        self.order = 2
 
     def apply_gaussian_noise(self):
         # noise = np.random.randn(*features.shape).astype(np.float32)
@@ -28,7 +28,7 @@ class DataAugmentation():
 
     def apply_lp(self):
         nyq = 0.5 * self.fs
-        normal_cutoff = self.cutoff / nyq
+        normal_cutoff = self.lp_cutoff / nyq
         # Get the filter coefficients
         b, a = butter(self.order, normal_cutoff, btype='lowpass', analog=False)
         y = filtfilt(b, a, self.audio)
@@ -36,7 +36,7 @@ class DataAugmentation():
 
     def apply_hp(self):
         nyq = 0.5 * self.fs
-        normal_cutoff = self.cutoff / nyq
+        normal_cutoff = self.hp_cutoff / nyq
         # Get the filter coefficients
         b, a = butter(self.order, normal_cutoff, btype='highpass', analog=False)
         y = filtfilt(b, a, self.audio)