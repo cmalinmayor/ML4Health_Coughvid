7d9b5d91291b77e3b37cd79831d6c48b707e51f7
cass-li <cassandralee729@gmail.com>
2021-12-05 00:36:00 -0500

    updated graph.py to align with new training log

--- scripts/graphs.py
+++ scripts/graphs.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.13.1
+#       jupytext_version: 1.13.2
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -35,9 +35,11 @@ def read_stats_from_log(filename):
 def get_stats_for_epoch(lines, start_index):
     result = {}
     curr_index = start_index
-    o, curr_index = get_line_1(lines, curr_index)
+    o, curr_index = get_line_0(lines, curr_index)
     if o is None:
         return None, curr_index
+    result.update(o)
+    o, curr_index = get_line_1(lines, curr_index)
     result.update(o)
     o, curr_index = get_line_2(lines, curr_index)
     result.update(o)
@@ -52,8 +54,8 @@ def get_stats_for_epoch(lines, start_index):
     return result, curr_index
         
         
-def get_line_1(lines, index):
-    line_re = 'epoch: (\d*)...train loss..(\d.\d*)..train acc..(\d.\d*)'
+def get_line_0(lines, index):
+    line_re = 'Report for epoch (\d*)'
     p = re.compile(line_re)
     match = None
     while(match is None and index < len(lines)):
@@ -64,9 +66,22 @@ def get_line_1(lines, index):
         return None, index
     # print(f"Found match at index {index}")
     epoch = match.group(1)
-    train_loss = float(match.group(2))
-    train_acc = float(match.group(3))
-    return {"epoch": epoch, "train_loss": train_loss, "train_acc": train_acc}, index
+
+    return {"epoch": epoch}, index
+
+def get_line_1(lines, index):
+    line_re = 'Train accuracy..(\d.\d*)'
+    p = re.compile(line_re)
+    match = None
+    while(match is None and index < len(lines)):
+        line = lines[index]
+        match = p.match(line)
+        index += 1
+    if match is None:
+        return None, index
+    train_accuracy = match.group(1)
+
+    return {"train accuracy": train_accuracy}, index
 
 def get_line_2(lines, index):   
     line_re = "Accuracy at threshold (\d.\d*)..(\d.\d*)"
@@ -144,19 +159,12 @@ def get_line_6(lines, index):
 
 
 # %%
+filename = '../da_log1_augmentation_result.txt'
 
-filename_augmentation = '../da_log1_augmentation_result.txt'
-filename_basic = '../train_100_epochs_filtered.log'
-
-basic_df = read_stats_from_log(filename_basic)
-aug_df = read_stats_from_log(filename_augmentation)
-
+df = read_stats_from_log(filename)
 
 # %%
-basic_df
-
-# %%
-aug_df
+df
 
 
 # %%
@@ -172,7 +180,7 @@ def plot_accuracy(df, title="Coswara Accuracy", max_epoch=100):
     source = ColumnDataSource(df)
     p = figure(title=title, x_axis_label='Epoch', y_axis_label='Accuracy')
 
-    p.line(x="epoch", y="train_acc", source=source, legend_label="train", line_width=2)
+    p.line(x="epoch", y="train accuracy", source=source, legend_label="train", line_width=2)
     p.line(x="epoch", y="balanced_test_acc", source=source, legend_label="test", line_width=2, color="red")
     p.xaxis.axis_label_text_font_size = '14pt'
     p.xaxis.major_label_text_font_size = '14pt'
@@ -187,5 +195,3 @@ def plot_accuracy(df, title="Coswara Accuracy", max_epoch=100):
 
 # %%
 plot_accuracy(basic_df, title="Baseline Accuracy During Training")
-
-# %%
